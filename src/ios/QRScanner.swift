018cd38458bc73b02d8b110bddf6677d7d705da2
Hannu Niemela <hannu.niemela@tieto.com>
2019-02-12 23:11:10 +0200

    DataMatrix type support added

--- src/ios/QRScanner.swift
+++ src/ios/QRScanner.swift
@@ -153,7 +153,7 @@ class QRScanner : CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
                 metaOutput = AVCaptureMetadataOutput()
                 captureSession!.addOutput(metaOutput)
                 metaOutput!.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-                metaOutput!.metadataObjectTypes = [AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeCode128Code]
+                metaOutput!.metadataObjectTypes = [AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeDataMatrixCode]
                 captureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
                 cameraView.addPreviewLayer(captureVideoPreviewLayer)
                 captureSession!.startRunning()
@@ -238,7 +238,7 @@ class QRScanner : CDVPlugin, AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         let found = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        if found.type == AVMetadataObjectTypeQRCode || found.type == AVMetadataObjectTypeEAN13Code || found.type == AVMetadataObjectTypeCode128Code && found.stringValue != nil {
+        if found.type == AVMetadataObjectTypeQRCode || found.type == AVMetadataObjectTypeEAN13Code || found.type == AVMetadataObjectTypeCode128Code || found.type == AVMetadataObjectTypeDataMatrixCode && found.stringValue != nil {
             scanning = false
             let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: found.stringValue)
             commandDelegate!.send(pluginResult, callbackId: nextScanningCommand?.callbackId!)
